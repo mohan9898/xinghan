6587b95f4e596b9ca2c82550f6ebabbd21669b21
零零豆 <29398217@qq.com>
2024-09-23 02:56:00 +0800

    Create yanchi.js

---
+++ alist/js/yanchi.js
@@ -0,0 +1,6 @@
+    let interval = setInterval(() => {
+        if (document.querySelector(".footer")) {
+            document.querySelector("#customize").style.display = "";
+            clearInterval(interval);
+        }
+    }, 200);
